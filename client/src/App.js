b9fa8bf6a2b8ae0729d83c12fd1fe33b4b4f6bf1
Dev Gajjar <dgajjar432@gmail.com>
2025-04-13 18:59:19 +0530

    fix issue

--- client/src/App.js
+++ client/src/App.js
@@ -29,7 +29,7 @@ function App() {
         <CssBaseline />
         <Routes>
           <Route element={<Layout />}>
-            <Route path="/" element={<Navigate to="/dashboard" replace />} />
+            <Route path="/" element={<Navigate to="/" replace />} />
             <Route path="/dashboard" element={<Dashboard />} />
             <Route path="/products" element={<Products />} />
             <Route path="/customers" element={<Customers />} />
